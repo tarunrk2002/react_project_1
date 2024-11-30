babac51b2f6485ba9dda4ca82043c233f02e9543
Tarun rk <tarunrk2002@gmail.com>
2024-11-30 15:20:32 +0530

    ..

--- mt-react-app/src/Card.jsx
+++ mt-react-app/src/Card.jsx
@@ -1,10 +1,15 @@
 function Card(){
    return(
-    <div className="card">
+    <>
+    <div className="small_card">
         <img src="public/vite.svg" alt="just a picture" />
         <h3>this is h3</h3>
         <p>this is a card</p>
-    </div>
+        </div>
+
+       
+        </>
+
    );
 }
 
